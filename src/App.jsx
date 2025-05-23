b3a65b87852d28de5301d21ba967fcb7778daa82
Jeiryuu <jayrmanabat3214@gmail.com>
2025-05-23 20:49:53 +0800

    test

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const audioRef = useRef(null)
+  const [showGift, setShowGift] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowGift(true)
+    }, 8000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      audioRef.current.play()
+    } else {
+      audioRef.current.pause()
+      audioRef.current.currentTime = 0
+    }
+  }
+
+  return (
+    <>
+      {!showGift && (
+        <div className="volume-warning">
+          <div className="volume-text">
+            <span>🔊</span> Increase your device’s volume for the best experience
+          </div>
+        </div>
+      )}
+
+      {showGift && (
+        <div className="birthday-gift">
+          <div className="gift">
+            <input id="click" type="checkbox" onChange={handleToggle} />
+            <label className="click" htmlFor="click"></label>
+            <div className="wishes">Happy Birthday!</div>
+            <div className="sparkles">
+              <div className="spark1"></div>
+              <div className="spark2"></div>
+              <div className="spark3"></div>
+              <div className="spark4"></div>
+              <div className="spark5"></div>
+              <div className="spark6"></div>
+            </div>
+            <audio ref={audioRef} src="\src\assets\moan.mp3" preload="auto" />
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default App
